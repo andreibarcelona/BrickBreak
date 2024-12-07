e74296fc6e2ac9fe1c7042b886f46023bb55508b
andreibarcelona <zianandrei280@gmail.com>
2024-12-07 06:07:14 +0000

    commit -m "submission 12/7/24"

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,119 @@
 import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
 
+/**
+ * Represents the ball used in the Brick Break game. The ball moves across the screen
+ * and interacts with the paddle, bricks, and boundaries.
+ */
 public class Ball {
 	//your code here!
+	private int Xpos;
+	private int Ypos;
+	private int size;
+	private int XVelocity;
+	private int YVelocity;
+	private int Velocity;
 	
-	//don't forget you need instance variables:
 	
-	
+
 	//constructor(s):
-	
+	public Ball (int Xpos, int Ypos, int size) {
+		this.Xpos = Xpos;
+		this.Xpos = Ypos; 
+		this.Velocity = 1;
+		this.XVelocity = 1;
+		this.YVelocity = 1; 
+		this.size = size;
+	}
+	//Getters & Setters:
+	public int getXpos() {
+		return Xpos;
+	}
+
+	public void setXpos (int Xpos) {
+		this.Xpos = Xpos;
+	}
+
+	public int getYpos() {
+		return Ypos;
+	}
+
+	public void setYpos(int Ypos) {
+		this.Ypos = Ypos;
+	}
+	public int getX() {
+		return Xpos;
+	}
+
+	public void setX (int Xpos) {
+		this.Xpos = Xpos;
+	}
+
+	public int getY() {
+		return Ypos;
+	}
+
+	public void setY(int Ypos) {
+		this.Ypos = Ypos;
+	}
+
+	public int getVelocity() {
+		return Velocity;
+	}
+
+	public void setXVelocity(int XVelocity){
+		this.XVelocity = XVelocity;
+	}
+
+	public void setYVelocity(int YVelocity){
+		this.YVelocity = YVelocity;
+	}
+
+	public int getSize() {
+		return size;
+
+	}
+
+
 	//methods:
 
+
+	 /**
+     * Precondition: A valid Graphics object `g` is provided.
+     * Postcondition: Draws the ball on the screen with the specified size and position.
+     */
+	public void draw(Graphics g) { 
+		g.setColor(Color.red); // call first to set the desired color
+		g.fillOval(Xpos, Ypos, size, size);
+	}
+
+
+	  /**
+     * Precondition: The game loop is running and calls this method to update the ball's position.
+     * Postcondition: Updates the ball's X and Y coordinates based on its velocity.
+     */
+	public void move(){
+		Xpos += XVelocity;
+		Ypos += YVelocity;
+	}
+
+    /**
+     * Precondition: The ball hits a horizontal surface.
+     * Postcondition: Reverses the ball's horizontal direction.
+     */
+	public void reverseX() {
+		XVelocity *= -1;
+	}
+
+	  /**
+     * Precondition: The ball hits a vertical surface.
+     * Postcondition: Reverses the ball's vertical direction.
+     */
+	public void reverseY() {
+	    YVelocity *= -1;
+	}
+
 }
+
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,101 @@
-import java.awt.Graphics;
 import java.awt.*;
 
+/**
+ * Represents the paddle used in the Brick Break game. The paddle can move horizontally
+ * within the game boundary based on user input and is used to deflect the ball.
+ */
 public class Paddle {
+	
+	
+	private int width;
+	private int height;
+	private int x;
+	private int y;
+	private int Velocity;
 
+
+	public Paddle (int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.Velocity = 0;
+
+	}
 	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+		public int getVelocity() {
+		return Velocity;
+	}
+
+	public void setVelocity(int Velocity){
+		this.Velocity = Velocity;
+	}
+
 	
 	//methods:
-	
-	
+
+
+	  /**
+     * Precondition: A valid Graphics object `g` is provided.
+     * Postcondition: Draws the paddle on the screen with the specified dimensions and position.
+     */
+	public void draw(Graphics g) { 
+		g.setColor(Color.red); // call first to set the desired color
+		g.fillRect(x , y, width,  height); // draws a rectangle
+	}
+
+
+	  /**
+     * Precondition: A valid integer `Velocity` is provided.
+     * Postcondition: Sets the paddle's velocity to the given value.
+     */
+	public void addVelocity(int Velocity){
+		this.Velocity = Velocity ;
+	}
+
+ /**
+     * Precondition: The game loop calls this method during the game's active state.
+     * Postcondition: Moves the paddle horizontally based on its velocity. Ensures the paddle
+     * stays within the screen boundaries.
+     */
+	public void move(){
+		x += Velocity;
+		if (x < 0) {
+            x = 0; // Left boundary
+        } else if (x + width > 692) { // screen width is 692
+            x = 692 - width;
+	}
+  }
 }
